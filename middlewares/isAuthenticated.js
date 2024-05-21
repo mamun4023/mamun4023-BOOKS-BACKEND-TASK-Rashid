27d73fcd8b929de56f92811ef9f078d182ec2d62
rashid4023 <mamunur@mds.com.bd>
2024-05-21 18:28:54 +0600

    add role middleware

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,8 +1,9 @@
+
 const jwt = require("jsonwebtoken");
 const catchError = require("../errorHandlers/errorCatcher");
 
-const isAuthenticated = async (req, res, next) => {
-    const token = req.cookies.token
+const isAuthenticatedUser = async (req, res, next) => {
+    const token = req.cookies.token;
     // console.log("token" , token)
     if (!token) return next(new catchError("Trying unauthorized access!", 404));
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
@@ -15,4 +16,12 @@ const isAuthenticated = async (req, res, next) => {
     });
 };
 
-module.exports = {isAuthenticated};
+const isAuthenticatedAdmin = async (req, res, next) => {
+    const user = req.user;
+    if (user.role != "admin") {
+        next(new catchError("You does not have authority to access!"));
+    }
+    next();
+};
+
+module.exports = { isAuthenticatedUser, isAuthenticatedAdmin };
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 
 const router = require('express').Router()
 const userControllers = require('../controllers/user')
-const {isAuthenticated} = require('../middlewares/isAuthenticated')
+const {isAuthenticatedUser, isAuthenticatedAdmin} = require('../middlewares/isAuthenticated')
 
 router.post('/signup', userControllers.signUp)
 router.post('/signin', userControllers.signIn)
 router.post('/logout', userControllers.logout)
 
-router.get('/test',  isAuthenticated, (req, res)=>{
+router.get('/test',  isAuthenticatedUser, isAuthenticatedAdmin, (req, res)=>{
     res.send("testing ")
 })
 
